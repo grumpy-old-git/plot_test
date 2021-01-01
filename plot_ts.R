5aebfd67aee5d3f770b16b274902b8e1206b00a9
Simon Brewer <brewersi@gmail.com>
2020-12-31 18:52:00 -0700

    Added annoy_brian argument

--- plot_ts.R
+++ plot_ts.R
@@ -1,5 +1,13 @@
-plot_ts <- function(x, y, ...) {
-  fit <- mgcv::gam(y ~ s(x))
-  plot(x, y, type = 'l', ...)
-  lines(x, fitted(fit), col = 2)
+plot_ts <- function(x, y, annoy_brian = FALSE, ...) {
+  library(ggplot2)
+  if (annoy_brian) {
+    ggplot(aes(x, y), data = data.frame(x, y)) + 
+      geom_line() + 
+      geom_smooth() +
+      theme_minimal()
+  } else {
+    fit <- mgcv::gam(y ~ s(x))
+    plot(x, y, type = 'l', ...)
+    lines(x, fitted(fit), col = 2, lwd = 2)
+  }
 }
